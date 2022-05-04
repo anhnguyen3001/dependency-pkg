ecd1e3e95c18d786bf6bdad6e5e1e347d4539c5f
Anh Nguyễn Hoàng <anh.nh1@teko.vn>
2022-05-04 11:08:09 +0700

    feat: v1.4.7

--- src/components/DetailCompany.tsx
+++ src/components/DetailCompany.tsx
@@ -1,11 +1,9 @@
 import React, { useEffect } from "react";
-import styled from "@emotion/styled";
 
 export const DetailCompany: React.FC = () => {
-  return <Wrapper>Detail Company</Wrapper>;
-};
+  useEffect(() => {
+    console.log("hook");
+  }, []);
 
-const Wrapper = styled.div`
-  background-color: red;
-  padding: 16px;
-`;
+  return <div>Detail Company</div>;
+};
